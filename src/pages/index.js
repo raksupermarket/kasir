672e6178fb2b2b02a4c5ae5f0404c3f925e942eb
raksupermarket <55552565+raksupermarket@users.noreply.github.com>
2020-01-22 05:44:39 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Cs from "../components/cs"
+
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
